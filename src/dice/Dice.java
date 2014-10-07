a5919197e3729014950a06cc9d75bf3153367028
coolrama <coolrama@gmail.com>
2014-10-08 02:11:57 +0530

    ll

--- src/dice/Dice.java
+++ src/dice/Dice.java
@@ -1,36 +1,31 @@
 package dice;
 
 import java.util.Random;
-import java.util.*;
 
 /**
  * Created by satyaman on 10/7/14.
  */
 public class Dice {
+    Random random = new Random();
 
-    public static int getDice(int start,int end)
+    public int getDice(int start, int end)
     {
         if(start >= end)
             throw new IllegalArgumentException("Start cannot exceed End.");
-        Random random = new Random();
-        long range = (long)end - (long)start +1;
-        log("Range:"+range);
-        long fraction = (long)(range * random.nextDouble());
-        log("fraction:"+fraction);
-
-
-        int dice = (int)(fraction + start);
+        int dice = random.nextInt(end);
         return dice;
-
     }
+
     public static void log(String message)
     {
         System.out.println(message);
     }
+
     public static void main(String args[])
     {
-        log("Dice:"+getDice(1,6));
-
+        Dice dice = new Dice();
+        log("Dice:"+dice.getDice(1,6));
+        log("Dice:"+dice.getDice(1,6));
     }
 }
 			
